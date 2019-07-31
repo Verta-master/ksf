43cd2486f71155424db766afab59d8c9c8e17236
Verta-master <vertyagina@gmail.com>
2019-07-31 13:18:50 +0600

    Update 2019-07-31T07:18:45.147Z

--- js/faq.js
+++
@@ -1,1 +0,0 @@
-$(".faq__question").click(function(){$(this).next().slideToggle(),$(this).parent().toggleClass("faq__item--open"),$(".faq__question").not(this).parent().removeClass("faq__item--open"),$(".faq__question").not(this).next().stop(!0,!0).slideUp()});
