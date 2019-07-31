181676639a44cf06fc9a744becef5db983ff5f72
Verta-master <vertyagina@gmail.com>
2019-07-31 13:18:30 +0600

    Animation

--- js/faq.js
+++
@@ -1,7 +0,0 @@
-//Accordion
-$('.faq__question').click(function() {
-  $(this).next().slideToggle();
-  $(this).parent().toggleClass('faq__item--open');
-  $('.faq__question').not(this).parent().removeClass('faq__item--open');
-  $('.faq__question').not(this).next().stop(true,true).slideUp();
-});
---
+++ js/main.js
@@ -0,0 +1,31 @@
+//Accordion
+$('.faq__question').click(function() {
+  $(this).next().slideToggle();
+  $(this).parent().toggleClass('faq__item--open');
+  $('.faq__question').not(this).parent().removeClass('faq__item--open');
+  $('.faq__question').not(this).next().stop(true,true).slideUp();
+});
+
+//Image rotation
+$(window).scroll(rotateHIWCircle);
+function rotateHIWCircle() {
+  $('.how__img img').css({ "transform": "rotate(" + $(this).scrollTop() * 2 + "deg)" })
+};
+
+//Show more buttons
+$('.show__btn').click(function() {
+  $('.show__item:nth-child(n + 10)').slideDown();
+  $(this).hide();
+});
+
+$('.recommend__link').click(function() {
+  $('.recommend__item:nth-child(n + 4)').slideDown();
+  $(this).hide();
+});
+
+$('.person__btn').click(function() {
+  $('.person__list li:nth-child(n + 3)').slideDown();
+  $('.person__list').addClass('person__list--open');
+  $('.person__quote').addClass('person__quote--open');
+  $(this).hide();
+});
