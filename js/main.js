ca503abef8c5e66f3bbe211ac23de836b80bb6ec
Verta-master <vertyagina@gmail.com>
2019-08-01 21:43:27 +0600

    Portfolio

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ function ajaxInit() {
   //Mobile menu
   $('.menu__btn--open').click(function(e) {
     e.stopPropagation();
-    $(this).next().slideDown();
+    $('.menu__list').slideDown();
     $(this).hide();
     $('.menu__btn--close').show();
     $('.logo').addClass('logo--open');
@@ -92,7 +92,7 @@ function ajaxInit() {
   
   $('.menu__btn--close').click(function(e) {
     e.stopPropagation();
-    $(this).next().slideUp();
+    $('.menu__list').slideUp();
     $(this).hide();
     $('.menu__btn--open').show();
     $('.logo').removeClass('logo--open');
