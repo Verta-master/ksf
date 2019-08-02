0eb3f240de1f8613d0e072c818cf7c32e8eef355
Verta-master <vertyagina@gmail.com>
2019-08-02 22:36:59 +0600

    Member

--- js/member.js
+++ js/member.js
@@ -23,4 +23,31 @@ function initTimer() {
 
 function ajaxInit() {
   initTimer();
+  
+  //Show more buttons
+  $('.recommend__link').click(function() {
+    $('.recommend__item:nth-child(n + 4)').slideDown();
+    $(this).hide();
+  });
+  
+  //Mobile menu
+  $('.menu__btn--open').click(function(e) {
+    e.stopPropagation();
+    $('.menu__list').slideDown();
+    $(this).hide();
+    $('.menu__btn--close').show();
+    $('.logo').addClass('logo--open');
+  });
+  
+  $('.menu__btn--close').click(function(e) {
+    e.stopPropagation();
+    $('.menu__list').slideUp();
+    $(this).hide();
+    $('.menu__btn--open').show();
+    $('.logo').removeClass('logo--open');
+  });
 }
+
+$(document).ready(function(){
+  ajaxInit();
+});
