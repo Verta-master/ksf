7c61f41b35846180cb8c99bf844a4abed3070607
Verta-master <vertyagina@gmail.com>
2019-08-02 11:12:32 +0600

    Blacklist

---
+++ js/blacklist.js
@@ -0,0 +1,40 @@
+function ajaxInit() {
+  // Blacklist Nav Button
+  $('.blacklist__nav li a').on('click', function(event) {
+    event.preventDefault();
+    var filter = $(this).attr('data-filter');
+
+    $('.blacklist__nav li a').removeClass('active');
+    $(this).addClass('active');
+
+    $('.blacklist__list li').removeClass('hidden');
+
+    if (filter !== 'all') {
+      var regex = new RegExp('\\b\\w*' + filter + '\\w*\\b');
+      $('.blacklist__list li').addClass('hidden').filter(function () {
+        return regex.test($(this).attr('data-filter'));
+      }).removeClass('hidden');
+    }
+  });
+  
+  //Mobile menu
+  $('.menu__btn--open').click(function(e) {
+    e.stopPropagation();
+    $('.menu__list').slideDown();
+    $(this).hide();
+    $('.menu__btn--close').show();
+    $('.logo').addClass('logo--open');
+  });
+  
+  $('.menu__btn--close').click(function(e) {
+    e.stopPropagation();
+    $('.menu__list').slideUp();
+    $(this).hide();
+    $('.menu__btn--open').show();
+    $('.logo').removeClass('logo--open');
+  });
+}
+
+$(document).ready(function(){
+  ajaxInit();
+});
--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,22 @@
-//Mobile menu
-$('.menu__btn').click (function(e) {
-  e.stopPropagation();
-  $('.menu__list').slideToggle();
-  $('menu').toggleClass('menu--opened');
+function ajaxInit() {
+  //Mobile menu
+  $('.menu__btn--open').click(function(e) {
+    e.stopPropagation();
+    $('.menu__list').slideDown();
+    $(this).hide();
+    $('.menu__btn--close').show();
+    $('.logo').addClass('logo--open');
+  });
+  
+  $('.menu__btn--close').click(function(e) {
+    e.stopPropagation();
+    $('.menu__list').slideUp();
+    $(this).hide();
+    $('.menu__btn--open').show();
+    $('.logo').removeClass('logo--open');
+  });
+}
+
+$(document).ready(function(){
+  ajaxInit();
 });
