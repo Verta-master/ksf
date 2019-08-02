66cc7daa103f5f8341c4e8124cffc48aef61a464
Verta-master <vertyagina@gmail.com>
2019-08-02 11:13:03 +0600

    Update 2019-08-02T05:12:58.355Z

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,1 @@
-$(".menu__btn").click(function(e){e.stopPropagation(),$(".menu__list").slideToggle(),$("menu").toggleClass("menu--opened")});
+function ajaxInit(){$(".menu__btn--open").click(function(o){o.stopPropagation(),$(".menu__list").slideDown(),$(this).hide(),$(".menu__btn--close").show(),$(".logo").addClass("logo--open")}),$(".menu__btn--close").click(function(o){o.stopPropagation(),$(".menu__list").slideUp(),$(this).hide(),$(".menu__btn--open").show(),$(".logo").removeClass("logo--open")})}$(document).ready(function(){ajaxInit()});
