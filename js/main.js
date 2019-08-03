55a70e0f940b5df8eddd05fd105d792123d8b0ef
Verta-master <vertyagina@gmail.com>
2019-08-03 18:01:40 +0600

    Update 2019-08-03T12:01:35.786Z

--- js/main.js
+++ js/main.js
@@ -39,6 +39,37 @@ $(document).ready(function(){
   ajaxInit();
 });
 
+function ajaxInit() {
+  //Accordion
+  $('.faq__question').click(function() {
+    $(this).next().slideToggle();
+    $(this).parent().toggleClass('faq__item--open');
+    $('.faq__question').not(this).parent().removeClass('faq__item--open');
+    $('.faq__question').not(this).next().stop(true,true).slideUp();
+  });
+  
+  //Mobile menu
+  $('.menu__btn--open').click(function(e) {
+    e.stopPropagation();
+    $('.menu__list').slideDown();
+    $(this).hide();
+    $('.menu__btn--close').show();
+    $('.logo').addClass('logo--open');
+  });
+  
+  $('.menu__btn--close').click(function(e) {
+    e.stopPropagation();
+    $('.menu__list').slideUp();
+    $(this).hide();
+    $('.menu__btn--open').show();
+    $('.logo').removeClass('logo--open');
+  });
+}
+
+$(document).ready(function(){
+  ajaxInit();
+});
+
 /*
  countdown.js v2.6.0 http://countdownjs.org
  Copyright (c)2006-2014 Stephen M. McKamey.
