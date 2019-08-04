c36a3be5cdd1042d0611e58bacae9de15bcd1038
Verta-master <vertyagina@gmail.com>
2019-08-04 20:28:44 +0600

    Update 2019-08-04T14:28:38.810Z

--- js/main.js
+++ js/main.js
@@ -284,3 +284,52 @@ $('.portfolio .portfolio__nav li a').on('click', function(event) {
     }).removeClass('hidden');
   }
 });
+
+//Sliders
+var mySwiper = new Swiper ('.certificate__slider1', {
+  pagination: {
+    el: '.certificate__pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.certificate__next',
+    prevEl: '.certificate__prev',
+  },
+  simulateTouch: false,
+  preventClicks: false,
+  preventClicksPropagation: false,
+  slidesPerView: 'auto',
+  spaceBetween: 25,
+});
+
+var mySwiper = new Swiper ('.certificate__slider2', {
+  pagination: {
+    el: '.certificate__pagination2',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.certificate__next2',
+    prevEl: '.certificate__prev2',
+  },
+  simulateTouch: false,
+  preventClicks: false,
+  preventClicksPropagation: false,
+  slidesPerView: 'auto',
+  spaceBetween: 25,
+});
+    
+var mySwiper = new Swiper ('.project__slider', {
+  navigation: {
+    nextEl: '.project__next',
+    prevEl: '.project__prev',
+  },
+  simulateTouch: false,
+  preventClicks: false,
+  preventClicksPropagation: false,
+});
+
+//Show more button
+$('.project__show').click(function() {
+  $('.project__list').slideDown();
+  $('.project__btns').hide();
+});
