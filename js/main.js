674b1a5977e21421854528cc0d8fe9ff3376a972
Verta-master <vertyagina@gmail.com>
2019-08-09 20:42:11 +0600

    Update 2019-08-09T14:42:03.603Z

--- js/main.js
+++ js/main.js
@@ -460,3 +460,9 @@ if (window.innerWidth < 480) {
     $(this).hide();
   });
 }
+
+if (window.innerWidth < 766) {
+  $("head").append('<meta  name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">');
+} else {
+  $("head").append('<meta name="viewport" content="width=device-width,initial-scale=0.7,shrink-to-fit=no,user-scalable=no,minimum-scale=0.5,maximum-scale=1.0">');
+}
---
+++ js/viewport.js
@@ -0,0 +1,1 @@
+window.innerWidth<766?$("head").append('<meta  name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">'):$("head").append('<meta name="viewport" content="width=device-width,initial-scale=0.7,shrink-to-fit=no,user-scalable=no,minimum-scale=0.5,maximum-scale=1.0">');
